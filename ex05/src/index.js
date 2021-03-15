fececcac150ea13947329e2db0086ca9d77d654f
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-15 21:03:52 +0100

    dress is in myDress

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+var dress = "little dress"
+// Only change code below this line
+function myDress() {
+    var dress = "ball gown";
+    
+// Only change code above this line
+    return dress;
+
+}
+// Only change code below this line
+myDress();
+// Only change code above this line
+console.log(myDress(dress));
+
+
+module.exports = myDress;
