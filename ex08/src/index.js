2ae6bcad3093c2cf844b964de5c8716fb066a903
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-15 23:21:50 +0100

    true -false

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,13 +1,10 @@
 var total = 0
 
 function funcWithArg(digit) {
-    return (digit + 9) / 5;
+    return (digit + 9) / 5
 }
 
 total = funcWithArg(6)
 
 var processed = 0
-
 console.log(total);
-
-module.exports = funcWithArg;
---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+
+function isLess(a, b) {
+    return (a < b);
+ 
+ 
+}
+    isLess(10, 15)
+    isLess(15, 10)
+    // Only change code above this line
+
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+
+
+module.exports = isLess;
