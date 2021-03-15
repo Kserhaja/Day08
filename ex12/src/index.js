56ead160e77d37b59f6c7ff55d038aa28a848f3e
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-16 00:21:59 +0100

    randomInteger done

---
+++ ex12/src/index.js
@@ -0,0 +1,11 @@
+function randomInteger() {
+    return Math.floor(Math.random() * 30);
+}
+randomInteger(0, 29);
+
+// Only change code below this line.
+
+console.log(randomInteger());
+
+
+module.exports = randomInteger;
