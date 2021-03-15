f07eef15021f3453295699883e3b276205e14b3b
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-15 22:33:21 +0100

    addFive

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+var sum = 0
+function addThree() {
+    sum = sum + 3;
+    console.log(sum);
+    // Only change code below this line
+}  
+addThree();
+    // Only change code below this line
+
+function addFive() {
+    sum = sum + 5
+   // Only change code below this line
+ 
+}
+addFive();
+
+
+console.log(sum);
+module.export = {
+    addThree,
+    addFive
+};
